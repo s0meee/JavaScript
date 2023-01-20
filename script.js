ad37473ea58073cb5ab5a1f55bc0f98efdbd1f4c
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-20 13:42:31 +0000

    qusetion2 Answer

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 20 + 30 //50
 “20” + “30” //2030
-“Hello” + " " + 2021 //Hell0 2021
-1 + 2 * 3 //7
+“Hello” + " " + 2021//Hello 2021
+1 + 2 * 3// 7
 (1 + 3) ** 2 //16
-1 / 0 //0
+1 / 0 //무한
 6 % 2 //0
-7.5 % 2 //1
+7.5 % 2 //1.5
 5 == 5 //true
 5 === 5 //true
-5 == “5” //true
-5 === “5” //false
-5 != 5.0 //true
-5 !== 5.0 //true
-“true” === true //false
+5 == “5” //error
+5 === “5” //error
+5 != 5.0 //false
+5 !== 5.0 //false
+“true” === true //error
 5 <= 5.0 //true
 5 >= 5 //true
 true || true //true
@@ -20,5 +20,5 @@ true || false //true
 true && true //true
 true && false //false
 !true //false
-!false //true
+!false //ture
 
