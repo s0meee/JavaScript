404ed19e65a2143c54500f9ced8ba38468021931
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-20 12:56:52 +0000

    Incremental operation

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let password = 2
-password = password > 3 && password >1
-
-console.log(password)
-// 하나 결과 값은 false 다른 하나 결과 값은 true 이기 때문에 and 연산하면 결과 값은 false
+let a =1
+a++ // a=a+1
+console.log(a) // 2
