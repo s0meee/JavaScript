09cac333bfaec28ad634282e7956fa45b985217b
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-20 13:30:06 +0000

    Solved qusetion2 (self)

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-let a = 1
-let b = 2 
-console.log(a,b) // 1,2 
-let c = a
-a = b
-b = c 
-console.log(a,b) //  2,1 
+20 + 30 //50
+“20” + “30” //2030
+“Hello” + " " + 2021 //Hell0 2021
+1 + 2 * 3 //7
+(1 + 3) ** 2 //16
+1 / 0 //0
+6 % 2 //0
+7.5 % 2 //1
+5 == 5 //true
+5 === 5 //true
+5 == “5” //true
+5 === “5” //false
+5 != 5.0 //true
+5 !== 5.0 //true
+“true” === true //false
+5 <= 5.0 //true
+5 >= 5 //true
+true || true //true
+true || false //true
+true && true //true
+true && false //false
+!true //false
+!false //true
 
