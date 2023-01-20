bc373334f34002719b7137fb2fd43d544a4b52d7
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-20 13:18:06 +0000

    Solved qusetion1 (self)

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let x = 2
-let y = 2
-console.log(x != y)//false
+let a = 1
+let b = 2 
+console.log(a,b) // 1,2 
+
+//1단계  a 30 b 50
+//2단계 b 50 temp 30 
+//3단계 a 50 temp 30
+//4단계 a 50 b 30 
+
+//a 1 b 2
+//b 2 t 1 // t =a (1)
+//a 2 t 1 // a = b(2)
+//a 2 b 1  // b=t(1)
+
+let temp = a
+a = b
+b = temp
+console.log(a,b) //  2,1 
+
