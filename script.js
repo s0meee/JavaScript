e47db80daa980dedb1a01c7d039b183dbfb27147
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-20 12:30:44 +0000

    Points to make mistakes: importance of allocation (=)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let password = 1
-password = password + 2
+password + 2
 
 console.log(password)
 
-//퀴즈 
-//정답 3
-// 1로 선언된 password 에다가 2를 더한 값이 할당됨으로 3이다.
+//결과 1 
+// 3이 아닌 이유 : password + 2 이 순간에만 더해짐 
+// 항상 = 를 활용해서 값을 할당해 줘야 함
