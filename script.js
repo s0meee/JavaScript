f51f9d73b1ac2545101276ebcd0c7496127f039b
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-21 12:15:37 +0000

    var, only the function is hosted as a local variable. The rest are all global variables. For statement, if statement, etc. are raised to global variables (excluding function)

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-var a =2 // 전역변수
-
-function foo () {
-  var b = 1
-} //지역변수
-
-console.log(b)
+for(var i = 1; i < 5; i++){
+  console.log(i)
+} //var i는 for문 안에서 선언이 됨
+console.log(i) //for입장에서 에러 일으키는게 맞음
