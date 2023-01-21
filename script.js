9f01b69c206e801aba2e5d11865ea4b882633e9b
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-21 15:04:39 +0000

    Array problem solving

--- script.js
+++ script.js
@@ -97,21 +97,36 @@ let animals= [
     "Zebra"
 ]
  
-//어레이에 마지막 아이템 “Zebra” 제거하기
+// 어레이에 마지막 아이템 "Zebra" 제거하기
+animals.pop()
+console.log(animals)
 
-//주어진 어레이에 “Dog” 추가하기
+// 주어진 어레이에 "Dog" 추가하기
+animals.push("Dog")
+console.log(animals)
 
-//주어진 어레이에 “Mosquito”,“Mouse”,“Mule” 추가하기
+//주어진 어레이에 "Mosquito","Mouse","Mule" 추가하기
+animals.push("Mosquito","Mouse","Mule")
+console.log(animals)
 
 //해당 어레이에는 "Human"이 있는가?
+console.log(animals.includes("Human"))
 
-//해당 어레이에는 “Cat” 이 있는가?
+//해당 어레이에는 "Cat" 이 있는가?
+console.log(animals.includes("Cat"))
 
-//"Red deer"을 "Deer"로 바꾸시오
+//"Red deer"을  "Deer"로 바꾸시오 
+animals[animals.indexOf("Red deer")] = "Deer"
+console.log(animals)
 
-//"Spider"부터 3개의 아이템을 기존 어레이에서 제거하시오
+//"Spider"부터 3개의 아이템을 기존 어레이에서 제거하시오 
+animals.splice(animals.indexOf("Spider"),3)
+console.log(animals)
 
-//"Tiger"이후의 값을 제거하시오
+// "Tiger"이후의 값을 제거하시오 
+animals.splice(animals.indexOf("Tiger"))
+console.log(animals)
 
 //"B"로 시작되는 아이템인 "Baboon"부터 "Bison"까지 가져와 새로운 어레이에 저장하시오
-
+let newList = animals.slice(animals.indexOf("Baboon"),animals.indexOf("Bison")+1)
+console.log(newList)
