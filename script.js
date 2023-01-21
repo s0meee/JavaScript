c27885abc2134a6247e6569695eae326aca27124
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-21 13:59:25 +0000

    I want to change the value: access index number and replace it

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ let fruit4 = "mango"
 let fruit5 = "grape"
 
 let fruit = ["banana","apple","Strawberry","mango","grape"]
-//위 코드들 한줄로 정리 
+
 console.log(fruit)
 console.log(fruit[3])
+
+fruit[0]="cherry" //fruit의 0번째 (바나나)를 체리로 바꾸기
+console.log(fruit)
+
+//값을 바꾸고 싶으면 인덱스 번호로 접근해서 바꾸면 됨 
