6a2eca18fea99f9009886ef60f9112d83353306a
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-22 05:09:20 +0000

    Object : Role of grouping related information and storing it as a single data

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let patient = {} //객체 {}안에 씀
-let patient = [] //배열 []안에 씀
+let patient = {
+  name : "somi",
+  age : 23,
+  disease : "cold"
+  
+} 
+
+console.log(patient)
+//patient에 대한 정보가 출력됨
+//관련된 정보를 묶어서 하나의 데이터로 저장해주는 역할 : 객체 
