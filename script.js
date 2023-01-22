e1f7cf7985dbb48f13680377e42763ccbddfbe76
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-22 06:14:10 +0000

    Add conditions. If statement is required, else if, else optional

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ let age = 18
 
 if(age > 20){
   console.log("운전이 가능합니다.")
+} else if(age >= 18) {
+  console.log("오토바이 운전이 가능합니다.")
 }else{
-  console.log("운전 불가능합니다.")
+  console.log("운전이 불가능합니다.")
 }
-//age 18살
-//18살은 20살 보다 크지 않음 > 조건에 대해 거짓임 >
-//else문에 있는 문장 실행 
-// 운전이 불가능합니다.
