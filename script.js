f830d56634bb24d84ef424612b128dab7c33c3b3
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-22 06:16:32 +0000

    Importance of conditional order

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let age = 18
+let age = 21
 
-if(age > 20){
-  console.log("운전이 가능합니다.")
-} else if(age >= 18) {
+if(age >= 18){
   console.log("오토바이 운전이 가능합니다.")
+} else if(age > 20) {
+  console.log("운전이 가능합니다.")
 }else{
   console.log("운전이 불가능합니다.")
 }
+//오토바이 운전이 가능합니다. 라고 나오는 이유 
+// if문도 참
+// else if문 도 참
+//if문 참 > 그 해당 문 실행 한다음 if문을 종료해버림
+//조건 범위가 있다면 범위를 작은것부터 큰것으로 두어야 함 
