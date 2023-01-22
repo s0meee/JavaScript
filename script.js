9bb61d59246ddfdcabe1dc861756cbbecfe06578
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-22 06:21:49 +0000

    If a sentence inside an if sentence Putting a condition in a condition

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 let age = 21
-//18 <= age < 20
-if(18 <= age && age < 20){
-  console.log("오토바이 운전이 가능합니다.")
-} else if(age > 20) {
-  console.log("운전이 가능합니다.")
+let licence = true
+
+if(age>20){
+  if(licence == true){
+    console.log("운전이 가능 합니다.")
+  }else{
+    console.log("면허를 따세용")
+  }
 }else{
   console.log("운전이 불가능합니다.")
 }
-//오토바이 운전이 가능합니다. 라고 나오는 이유 
-// if문도 참
-// else if문 도 참
-//if문 참 > 그 해당 문 실행 한다음 if문을 종료해버림
-//조건 범위가 있다면 범위를 작은것부터 큰것으로 두어야 함 
-//&& 그리고라는 뜻, 앞도 참이고 뒤에도 참이어야 참이 된다.
