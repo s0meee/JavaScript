25f11786dffd14b3434800967408ce06c39ccd82
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-22 05:32:34 +0000

    Putting objects in an array, leveraging them

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ console.log(patient["name"])
 patient.name = "sarah"
 patient["age"] = "20"
 console.log(patient)
+
+let patientList = [{name:"somee",age: 23},{name:"rabbit",age: 17},{name:"sarah",age: 17}]
+
+console.log(patientList)
+console.log("첫번째 환자는 : ",patientList[0])
+console.log("첫번째 환자는 : ",patientList[0].age)
+console.log("첫번째 환자는 : ",patientList[0]["age"])
